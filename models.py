3553410f7208d52b92a8234060f8c30dad1b491a
addityaasharma <adityasharma96655@gmail.com>
2025-09-16 15:12:50 +0530

    changes done

--- models.py
+++ models.py
@@ -41,7 +41,6 @@ class ChatGroup(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     chatTitle = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    panel = db.relationship("UserPanel", back_populates="chat_groups")
     chats = db.relationship("UserChat", back_populates="group", cascade="all, delete-orphan")
 
 class UserChat(db.Model):
