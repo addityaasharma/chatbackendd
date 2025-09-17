13d91a3975e5346b5f27cdc0d416ef9276f25792
addityaasharma <adityasharma96655@gmail.com>
2025-09-17 10:16:07 +0530

    hosted

--- socket_instance.py
+++ socket_instance.py
@@ -1,3 +1,10 @@
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room
 
 socketio = SocketIO(cors_allowed_origins="*")
+
+@socketio.on("join")
+def on_join(data):
+    room = data.get("room")
+    if room:
+        join_room(room)
+        print(f"User joined room: {room}")
