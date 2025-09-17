87e086de06e14019099fcad5a4fcfcdf671f72e4
addityaasharma <adityasharma96655@gmail.com>
2025-09-17 12:35:50 +0530

    changes done

--- app.py
+++ app.py
@@ -37,17 +37,23 @@ import eventlet
 eventlet.monkey_patch()
 
 from flask import Flask
+from config import Config
+from models import db
 from flask_cors import CORS
 from flask_migrate import Migrate
-from models import db
 from userRouter import userBP
 from socket_instance import socketio
-import os
 
 app = Flask(__name__)
-app.config.from_object("config.Config")
+app.config.from_object(Config)
 
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+CORS(
+    app,
+    resources={r"/*": {"origins": "*"}},
+    supports_credentials=True,
+    allow_headers=["Content-Type", "Authorization"],
+    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+)
 
 db.init_app(app)
 migrate = Migrate(app, db)
@@ -55,10 +61,13 @@ migrate = Migrate(app, db)
 app.register_blueprint(userBP, url_prefix="/user")
 socketio.init_app(app, cors_allowed_origins="*")
 
+
 @app.route("/")
-def index():
+def method_name():
     return "Hello"
 
+
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host="0.0.0.0", port=port, debug=True)
